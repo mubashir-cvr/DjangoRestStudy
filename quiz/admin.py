675c43e299c7fb94a62b5592580ae732d2dc6d12
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-22 00:26:40 +0530

    Designed Admin Page

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+@admin.register(models.Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display =[
+        'name',
+
+    ]
+
+@admin.register(models.Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display=[
+        'id',
+        'title',
+    ]
+class AnswerInlineModel(admin.TabularInline):
+    model=models.Answer
+    fields=[
+        'answe_text ',
+        'is_right',
+    ]
+
+@admin.register(models.Questions)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'quiz',
+        ]
+    list_display = [
+        'title', 
+        'quiz',
+        'date_updated'
+        ]
+    inlines = [
+        AnswerInlineModel, 
+        ] 
