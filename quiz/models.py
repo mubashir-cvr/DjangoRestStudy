a36de82b225f29ca9b0ee120205347c94207d6b7
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-21 23:59:11 +0530

    Updated Model

--- quiz/models.py
+++ quiz/models.py
@@ -38,8 +38,23 @@ class Questions(Updated):
         (4,_('Expert')),
 
     )
+    TYPES=(
+        (0,_('Multiple Choice')),
+    )
     quiz =models.ForeignKey(Quizzes,related_name='question',on_delete=models.DO_NOTHING)
-
+    technique = models.IntegerField(choices=TYPES,default=0,verbose_name=_("Type of Question"))
+    title = models.CharField(max_length=225,verbose_name=_("Title"))
+    difficulty = models.IntegerField(choices=SCALE,default=0,verbose_name=_("Difficulty"))
+    date_created =models.DateTimeField(auto_now_add=True,verbose_name=_("Date Created"))
+    is_active =models.BooleanField(default=False,verbose_name=_("Active Status"))
+    def __str__(self):
+        return self.title
 
 class Answer(Updated):
+    class Meta:
+        verbose_name =_("Answer")
+        verbose_name_plural =_("Answers")
+        ordering = ['id']
     question =models.ForeignKey(Questions,related_name='answer',on_delete=models.DO_NOTHING)
+    answe_text= models.CharField(max_length=225,verbose_name=_("Answer Text"))
+    is_right=models.BooleanField(default=False)
